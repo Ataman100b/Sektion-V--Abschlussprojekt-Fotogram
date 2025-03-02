bb5915097677771ccd3c4abd316ee9d3f0d336eb
Your Name <your.email@example.com>
2025-03-02 17:09:13 +0100

    Updates script.js to add an overlay/dialog feature when the user clicks on a specific element.

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
 
 
+template
 
+
+
+Overlay bzw. Dialog erstellen
+<style>
+    .overlay {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, 0.5);
+        
+    }
+
+    .d_none {
+        display: none;
+
+<style>
+<script>
+
+    function toggleOverlay() {
+    let overlayRef = document.getElementById('overlay');
+    overlayRef.classList.toggle('d_none');
+    }
+
+</script>
+
+// /*html*/
+<div onclick="toggleOverlay()" id=overlay class = overlay >Click me</div>
