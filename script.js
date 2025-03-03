f56c6c316649f500f5aae4851e1796b1d89ae559
Your Name <your.email@example.com>
2025-03-03 12:48:12 +0100

    Refactors header logo styling
    Updates width and margin-left properties to use the `clamp()` function for responsive design
    
    Refactors header logo styling for responsive design using `clamp()`
    
    Updates width and margin-left properties of the header logo to use the `clamp()` function, ensuring responsive design.
    
    Fixes #N/A

--- script.js
+++ script.js
@@ -31,3 +31,68 @@ Overlay bzw. Dialog erstellen
 
 // /*html*/
 <div onclick="toggleOverlay()" id=overlay class = overlay >Click me</div>
+
+
+
+
+
+
+---------------------------------------------
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Render Doors</title>
+    <style>
+        .doors {
+            display: inline-block;
+            width: 50px;
+            height: 50px;
+            margin: 5px;
+            background-color: lightblue;
+            text-align: center;
+            line-height: 50px;
+            border: 1px solid black;
+        }
+    </style>
+</head>
+<body>
+
+  
+
+
+  // call with Giuliano
+
+// css : use wrap
+
+
+
+let images = [
+
+"./img/door1.jpg",
+
+];
+
+
+
+    <script>
+        function renderDoors() {
+            for (let i = 1; images.lenght +1 ; i++) {  
+                document.getElementById('content').innerHTML += /*html*/ `
+                <div id="door${i+1}" onclick="openDoor(${i + 1})"   class='doors'>
+
+            `}
+
+            function openDoors(1) {
+                console.log('Door ${i+1} was clicked');
+                document.getElementById(
+                `door${i+1}`
+                ).style.backgroundImage = 'url('./img/cst{i).jpg')';
+            }
+        }
+    </script>
+
+</body>
+</html>
